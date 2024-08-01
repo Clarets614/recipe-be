f7e1bfa1ccec5aaae0717161e8f771c9140d4b55
Artemis <clare.stock@gmail.com>
2024-08-01 18:04:00 -0400

    changes to Ingredients table and created ingredients APIs

--- Recipeback/Controllers/IngredientController.cs
+++ Recipeback/Controllers/IngredientController.cs
@@ -10,20 +10,47 @@ namespace Recipeback.Controllers
     {
         StockRecipesContext dbContext = new StockRecipesContext();
 
-        [HttpPost()]
+        [HttpPost("name")]
         public IActionResult AddIngredient(Ingredient name)
         {
+            if(name == null)
+            {
+                return BadRequest();
+            }
             dbContext.Ingredients.Add(name);
             dbContext.SaveChanges();
             return Created($"api/Ingredient/{name.Id}", name);
 
         }
-
         [HttpGet()]
+        public IActionResult GetAllIngredients()
+        {
+            List<Ingredient> result = dbContext.Ingredients.ToList();
+            return Ok(result);
+        }
+
+        [HttpGet("recipe")]
         public IActionResult GetIngredients(string recipe)
         {
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+            {
+                
+            }
             List<Ingredient> ingredient = dbContext.Ingredients.Where(x => x.Recipe == recipe).ToList();
             return Ok(ingredient);
+        }
+
+        [HttpDelete("{name}")]
+        public IActionResult RemoveIngredient(string name)
+        {
+            if (string.IsNullOrEmpty(name)) { return NotFound(); }
+            Ingredient ingredient = dbContext.Ingredients.FirstOrDefault(x => x.Itemname == name);
+            dbContext.Ingredients.Remove(ingredient);
+            dbContext.SaveChanges();
+            return NoContent();
         }
     }
 }
